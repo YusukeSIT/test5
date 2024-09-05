bd48853891a7b62ba3c58799ffd221203fc9d325
YusukeSIT <151612445+YusukeSIT@users.noreply.github.com>
2024-09-06 02:31:47 +0900

    Add files via upload

--- data/tsts.js
+++ data/tsts.js
@@ -25,7 +25,7 @@ new_gltf_model.setAttribute('box-colider', '');
 document.querySelector('a-scene').appendChild(new_gltf_model);
 
 new_assets_item = document.createElement('a-assets-item');
-new_assets_item.setAttribute('id', 'model-tube');
+new_assets_item.setAttribute('id', '#model-tube');
 new_assets_item.setAttribute('src', './data/tube.glb');
 
 new_assets.appendChild(new_assets_item);
