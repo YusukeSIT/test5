f7b4c2e9cedf33744db4e45c6c5851390ee7e85a
YusukeSIT <151612445+YusukeSIT@users.noreply.github.com>
2024-09-06 14:10:33 +0900

    Add files via upload

--- data/uiController.js
+++ data/uiController.js
@@ -2,14 +2,16 @@ let uiButton = document.getElementById("UIVisibilityButton");
 let sideUI = document.getElementById("SideUI");
 var visible = true;
 function changeUI() {
-	if (visible) {
+	if (visible == true) {
 		sideUI.setAttribute('style', 'display:none');
 		uiButton.setAttribute('value', '≫');
+		uiButton.setAttribute('style', 'float:left');
 		visible = false;
 	}
-	if (!visible) {
+	else if (visible == false) {
 		sideUI.setAttribute('style', 'display:block');
 		uiButton.setAttribute('value', '≪');
+		uiButton.setAttribute('style', 'float:right');
 		visible = true;
 	}
 }
